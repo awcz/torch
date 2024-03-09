1f597f7184eaa30a157a1ee91b8b94208838f12e
awcz <20097742+awcz@users.noreply.github.com>
2024-03-09 16:17:42 +0100

    upgrade korge to 5.4.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     repositories { mavenLocal(); mavenCentral(); google(); gradlePluginPortal() }
 
     dependencies {
-        classpath("com.soywiz.korge.settings:com.soywiz.korge.settings.gradle.plugin:5.3.2")
+        classpath("com.soywiz.korge.settings:com.soywiz.korge.settings.gradle.plugin:5.4.0")
     }
 }
 
